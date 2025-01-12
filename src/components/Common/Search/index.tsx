0c28988247a8a01db9adb5f233894079f34a43bf
f.e.h.farshad <f.e.h.farshad@gmail.com>
2025-01-12 10:58:33 +0330

    feat: add search container

--- src/components/Common/Search/index.tsx
+++ src/components/Common/Search/index.tsx
@@ -1,23 +1,50 @@
-import React, { useState } from "react"
-import { Text } from "../Form/Text"
-import { useSelector } from "react-redux"
-import { RootState } from "../../../app/store"
+import React, { useState } from "react";
+import { Text } from "../Form/Text";
+import { useSelector } from "react-redux";
+import { RootState } from "../../../app/store";
+import { IPost } from "../../../interfaces/posts";
 
 export const Search = () => {
-    const [query, setQuery] = useState<string>('')
-    const { posts } = useSelector((state: RootState) => state.posts)
-    let timer: NodeJS.Timeout
+  const [query, setQuery] = useState<string>("");
+  const { posts } = useSelector((state: RootState) => state.posts);
+  const [filteredPosts, setFilteredPosts] = useState<IPost[]>([]);
+  let timer: NodeJS.Timeout;
 
-    const search = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const search = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(e.target.value);
+    const filtered = posts.filter((post) => {
+      return post.title.includes(query);
+    });
+    setFilteredPosts(filtered);
+  };
 
-    }
-
-    return (
-        <div className="relative">
-            <Text classes="w-full" type="text" placeholder="جستجو کنید ..." onChange={search} value={query} name="serach" />
-            <div className="absolute text-black bg-slate-200 z-10 mt-1 rounded-md w-full p-2">
-                test
-            </div>
+  return (
+    <div className="relative">
+      <Text
+        classes="w-full"
+        type="text"
+        placeholder="جستجو کنید ..."
+        onChange={search}
+        value={query}
+        name="serach"
+      />
+      {query ? (
+        <div className="absolute text-black bg-slate-100 z-10 mt-1 rounded-md w-full max-h-60 overflow-auto shadow-lg">
+          {filteredPosts.map((post: IPost) => {
+            return (
+              <div className="flex items-center gap-2 m-2">
+                <span
+                  className="bg-orange-500 w-1 h-1 rounded-lg"
+                  id="indicator"
+                ></span>
+                <p className="cursor-pointer w-full" key={post.id}>
+                  {post.title}
+                </p>
+              </div>
+            );
+          })}
         </div>
-    )
-}
+      ) : null}
+    </div>
+  );
+};
